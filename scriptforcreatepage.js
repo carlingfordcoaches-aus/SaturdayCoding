9e34f667a7f9008ffa0fafc1b59bb68b86383f96
Aarya <aarya.dave123@gmail.com>
2025-11-01 11:24:36 +1100

    added some boiler plate code

---
+++ scriptforcreatepage.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("submit")
+
+button.addEventListener("click", () => {
+    const question1 = document.getElementById("question1").value
+    alert(question1)
+    // alert("Hello There")
+})
