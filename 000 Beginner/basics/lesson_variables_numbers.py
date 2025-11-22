a2eaa720935c9d73469b48a22e756452b6f78795
carlingfordcoaches-aus <carlingfordcoaches@gmail.com>
2025-11-22 12:38:34 +1100

    progress tracking
    seeing how much progress has been done and how much time was studying

--- 000 Beginner/basics/lesson_variables_numbers.py
+++ 000 Beginner/basics/lesson_variables_numbers.py
@@ -3,12 +3,17 @@
 Focus: Manage integers and floats to track classroom progress."""
 
 def exercise_1():
+    done = int(input("How many lessons out of 10 have you completed? "))
+    percent = (done/10) * 100
+    print(f"Progress: {done}/10 ({percent}%)")
     """Track completed and remaining lessons, then print a friendly progress bar."""
-    # TODO: Track completed and remaining lessons, then print a friendly progress bar.
+    # TODO: Track completed and remaining lessons, then print a friendly progress bar
     pass
 
-
 def exercise_2():
+    min = int(input('how long did you study for in min: '))
+    hour = (min/60)
+    print(f"Time: {min} minutes = {hour} hours ")
     """Convert study minutes to hours with a formatted sentence."""
     # TODO: Convert study minutes to hours with a formatted sentence.
     pass
@@ -17,7 +22,7 @@ def exercise_2():
 def main():
     """Uncomment the exercises you want to run."""
     # exercise_1()
-    # exercise_2()
+    exercise_2()
     pass
 
 if __name__ == '__main__':
