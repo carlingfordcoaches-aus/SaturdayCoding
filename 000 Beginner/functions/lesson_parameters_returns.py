d4fcac224d50a580db634c42b9ce3e1db70e96c3
carlingfordcoaches-aus <carlingfordcoaches@gmail.com>
2026-05-21 18:36:34 +1000

    21/05/2026
    coding

--- 000 Beginner/functions/lesson_parameters_returns.py
+++ 000 Beginner/functions/lesson_parameters_returns.py
@@ -15,13 +15,19 @@ def exercise_1():
 def exercise_2():
     """**Make Badge**: Write a function `make_badge(name, club="Coding Crew")` that takes a name and an optional club name and returns a formatted badge label."""
     # TODO: Write make_badge(name, club="Coding Crew") that formats a badge label.
+    def make_badge(name):
+        return name    
+        print(make_badge(name))
+    
     pass
 
 
 def main():
     """Uncomment the exercises you want to run."""
-    exercise_1()
-    # exercise_2()
+    # exercise_1()
+    exercise_2()
+  
+    
     pass
 
 if __name__ == '__main__':
